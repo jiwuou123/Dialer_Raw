8ca868721532948e74d0078bea2e734153d600a7
杨帆 <yangfan@oaserver.dw.gdbbk.com>
2016-07-27 17:21:52 +0800

    处理menu键 跳转到系统设置

--- app/src/main/java/com/android/dialer/TestFragmentActivity.java
+++ app/src/main/java/com/android/dialer/TestFragmentActivity.java
@@ -1,21 +1,25 @@
 package com.android.dialer;
 
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telecom.TelecomManager;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 
 import com.android.contacts.common.activity.TransactionSafeActivity;
 import com.android.contacts.common.interactions.TouchPointManager;
+import com.android.dialer.m1000systemdialog.SetupDialog;
 
 /**
  * Created by Administrator on 2016/7/26.
  */
 public class TestFragmentActivity  extends TransactionSafeActivity {
     private static final String TAG_Dialtacts_Fragment = "dialtacts_fragment";
-    private DialtactsFragment mDlf = null;
+    private DialtactsFragment mDlf = null;//主fragment
+    private SetupDialog mSetupdialog = null;//设置dialog
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +40,24 @@ public class TestFragmentActivity  extends TransactionSafeActivity {
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
             moveTaskToBack(false);
+            return true;
+        }else if (keyCode == KeyEvent.KEYCODE_MENU) {
+            // 弹出设置按钮
+
+            SetupDialog.Builder customBuilder = new SetupDialog.Builder(this);
+            mSetupdialog = customBuilder.create();
+            customBuilder.setNegativeButton(new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent callSettingsIntent = new Intent(TelecomManager.ACTION_SHOW_CALL_SETTINGS);
+                    callSettingsIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(callSettingsIntent);
+                    mSetupdialog.dismiss();
+                }
+            });
+            mSetupdialog = customBuilder.create();
+            mSetupdialog.show();
+
             return true;
         }
         return super.onKeyDown(keyCode, event);
